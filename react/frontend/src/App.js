a498cd6a46f1beee2ab6fb7afd873908d9d7ba95
Dwight Chesbro <dwight@fathom.company>
2022-04-16 14:59:52 -0700

    Moving more markup to separate components.

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { ClientSocket, socket } from './context/ClientSocket';
 
 // ...
-import { Col, Divider, Row, Table } from 'antd';
+import { Divider } from 'antd';
 
 // ...
-import ContestantDetails from './components/ContestantDetails';
-import RatingForm from './components/RatingForm';
-import RatingSummary from './components/RatingSummary';
+import ClientBallot from './components/ClientBallot';
+import ClientScorecard from './components/ClientScorecard';
 
 import './App.less';
 
@@ -15,34 +14,8 @@ function App() {
   const [categories, setCategories] = useState([]);
   const [contestants, setContestants] = useState([]);
 
-  const columns = [
-    {
-      dataIndex: ['artist', 'country', 'title'],
-      key: 'contestant',
-      render: (value, record) => (
-        <ContestantDetails contestant={record} />
-      ),
-      showSorterTooltip: false,
-      sorter: (a, b) => a.country.localeCompare(b.country),
-      title: 'Contestant',
-    },
-    {
-      dataIndex: 'total',
-      key: 'total',
-      showSorterTooltip: false,
-      sorter: (a, b) => a.total - b.total,
-      title: 'Total',
-    },
-    Table.EXPAND_COLUMN,
-  ];
-
-  const ballotSubmit = (values) => {
-    // socket.emit('clientBallotSubmit', values);
-    console.log('Success:', values);
-  };
-
   useEffect(() => {
-    socket.on('appBallotTallied', (data) => {
+    socket.on('appBallotTally', (data) => {
       console.log(data);
     });
     
@@ -57,49 +30,28 @@ function App() {
 
   return (
     <ClientSocket.Provider value={socket}>
-      <div style={{margin: '1rem auto', maxWidth: 768 }}>
-        
-        <Divider orientation="left">Client ballot</Divider>
-        <div id="clientBallot" style={{margin: 'auto', maxWidth: 384 }}>
-          <ContestantDetails
-            contestant={
-              {
-                country: 'Country',
-                code: 'gb',
-                artist: 'Artist',
-                title: 'Title',
-                representative: '',
-              }
+      <Divider orientation="left">Client ballot</Divider>
+      <div className="tv-uiPreview">
+        <ClientBallot
+          categories={categories}
+          contestant={
+            {
+              country: 'Country',
+              code: 'gb',
+              artist: 'Artist',
+              title: 'Title',
+              representative: '',
             }
-          />
-          <RatingForm categories={categories} onFinish={ballotSubmit} />
-        </div>
-        
-        <Divider orientation="left">Client scorecard</Divider>
-        <div className="clientScorecard">
-          <Table
-            columns={columns}
-            dataSource={contestants}
-            expandable={{
-              expandedRowRender: (record) => (
-                <div className="">
-                  <Row gutter={[16, 16]}>
-                    {categories.map((category) => (
-                      <Col key={category.key} md={6} xs={12}>
-                        <RatingSummary
-                          category={category}
-                          value={Math.floor(Math.random() * category.max)}
-                        />
-                      </Col>
-                    ))}
-                  </Row>
-                </div>
-              ),
-              // rowExpandable: (record) => record.total,
-            }}
-            pagination={false}
-          />
-        </div>
+          }
+        />
+      </div>
+      
+      <Divider orientation="left">Client scorecard</Divider>
+      <div className="tv-uiPreview">
+        <ClientScorecard
+          categories={categories}
+          contestants={contestants}
+        />
       </div>
     </ClientSocket.Provider>
   );
