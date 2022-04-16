8b34431ca038ebbfc40fbd98c2c083efd5986f90
Dwight Chesbro <dwight@fathom.company>
2022-04-16 14:57:16 -0700

    Better layout on small screen devices.

--- react/frontend/src/components/ContestantDetails.js
+++ react/frontend/src/components/ContestantDetails.js
@@ -4,16 +4,16 @@ export default function ContestantDetails({ contestant }) {
   const { Text, Title } = Typography;
 
   return (
-    <Row className="tv-contestantDetails">
-      <Col className="tv-contestantDetails__flag">
-        <img alt="" src={process.env.PUBLIC_URL + '/media/flags/' + contestant.code + '.svg'} width="48px" />
+    <Row className="tv-contestantDetails" wrap={false}>
+      <Col className="tv-contestantDetails__flag" flex="2.875rem">
+        <img alt="" src={process.env.PUBLIC_URL + '/media/flags/' + contestant.code + '.svg'} />
       </Col>
-      <Col className="tv-contestantDetails__content">
+      <Col className="tv-contestantDetails__content" flex="auto">
         <Title level={5}>
           {contestant.country}
         </Title>
         <Text type="secondary">
-          {[contestant.artist, contestant.title].join(' – ')}
+          {contestant.artist} – "{contestant.title}"
         </Text>
       </Col>
     </Row>
