91f051635765f397f18f945f637ce511fb43850b
Dwight Chesbro <dwight@fathom.company>
2022-04-16 22:03:30 -0700

    Working on Graham Norton bitch button.

---
+++ react/frontend/src/components/GNBB.js
@@ -0,0 +1,9 @@
+export default function GNBB() {
+  return (
+    <div class="tv-GNBB">
+      <button type="button">
+        <img alt="" src={process.env.PUBLIC_URL + '/media/gnbb.png'} />
+      </button>
+    </div>
+  );
+}
