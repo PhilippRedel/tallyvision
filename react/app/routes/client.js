070a854681539ad4fa58e94f0da3491f73b1985e
Dwight Chesbro <dchesbro@gmail.com>
2022-04-16 22:01:03 -0700

    No longer sending category/contestant data.

--- react/app/routes/client.js
+++ react/app/routes/client.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET client interface */
+/* client GET route */
 router.get('/', (req, res, next) => {
-  res.send({
-    categories: res.app.get('categories'),
-    contestants: res.app.get('contestants'),
-  }).status(200);
+  res.send({ Message: 'Socket to me!' }).status(200);
 });
 
 module.exports = router;
