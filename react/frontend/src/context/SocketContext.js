8e5802388f4714c82d512705aaa64ea9fa4f23ae
Dwight Chesbro <dwight@fathom.company>
2022-05-05 15:17:49 -0700

    Added app and ballot context providers.

--- react/frontend/src/context/SocketContext.js
+++ react/frontend/src/context/SocketContext.js
@@ -1,3 +1,8 @@
+import { io } from 'socket.io-client';
 import React from 'react';
 
+export const ioClient = io('http://localhost:3030/client', {
+  autoConnect: false,
+});
+export const ioHost = io('http://localhost:3030/host');
 export const SocketContext = React.createContext();
