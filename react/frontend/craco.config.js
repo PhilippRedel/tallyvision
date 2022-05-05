57cf29ec9aa2059c1607bbcaf0e2610b61cc56a1
Dwight Chesbro <dwight@fathom.company>
2022-05-05 15:16:36 -0700

    Replacing some default Ant Design styles.

--- react/frontend/craco.config.js
+++ react/frontend/craco.config.js
@@ -9,10 +9,11 @@ module.exports = {
           lessOptions: {
             modifyVars: {
               '@btn-border-radius-base': '0.334rem',
+              '@btn-text-hover-bg': 'rgba(0, 0, 0, 0.05)',
               '@card-padding-base': '1rem',
+              '@card-radius': '1rem',
               '@primary-color': '#9254de',
               '@table-border-radius-base': '1rem',
-              '@card-radius': '1rem',
             },
             javascriptEnabled: true,
           },
