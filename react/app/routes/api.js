ca3f2f2719d9e45f8d21f750ea2ddf6c903d0157
Dwight Chesbro <dchesbro@gmail.com>
2022-05-11 15:27:06 -0700

    Revert "Pulling react branch into master."

--- react/app/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET api */
-router.get('/', function(req, res, next) {
-  res.send({ response: 'socket to me!' }).status(200);
-});
-
-module.exports = router;
